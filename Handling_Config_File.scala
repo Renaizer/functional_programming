66967267571cb7239b939811e3d5a532cfbe4a27
Hamidkhan Jahagirdar <hamidsheaven@gmail.com>
2022-03-19 20:01:44 +0530

    Handling_Config_File.scala
    Handling_Config_File.scala

---
+++ Handling_Config_File.scala
@@ -0,0 +1,19 @@
+import com.typesafe.config.{Config, ConfigFactory, ConfigResolveOption}
+
+val config : Config = ConfigFactory.parseFile(new File(configPath)).resolve
+
+config.getString("step1.step2.step3")
+config.getConfig("myConfig")
+
+/*
+Example.conf
+
+step1{
+  step2{
+    step3{
+      config1: 
+      config2: 
+    }
+  }
+}
+*/
